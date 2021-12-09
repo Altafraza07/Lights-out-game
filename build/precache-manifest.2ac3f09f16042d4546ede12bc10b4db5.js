49717b8715916d25ba2979c353d348a4d4cf12ee
Altaf Raza <altafrzakgn@gmail.com>
2021-12-09 18:59:41 +0530

    fixed homepage

---
+++ build/precache-manifest.2ac3f09f16042d4546ede12bc10b4db5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "80b1c638e32e4dd8028d",
+    "url": "/Lights-out-game/static/css/main.807ac6b1.chunk.css"
+  },
+  {
+    "revision": "80b1c638e32e4dd8028d",
+    "url": "/Lights-out-game/static/js/main.80b1c638.chunk.js"
+  },
+  {
+    "revision": "b43a7eab4c27c21e54e5",
+    "url": "/Lights-out-game/static/js/1.b43a7eab.chunk.js"
+  },
+  {
+    "revision": "296c435d7cb0407de778",
+    "url": "/Lights-out-game/static/js/runtime~main.296c435d.js"
+  },
+  {
+    "revision": "639f6d2d002f2bcede1786db90b449d2",
+    "url": "/Lights-out-game/index.html"
+  }
+];
